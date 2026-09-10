1940f860958c46bf3d2cd57403f41c89fa2e7f9c
Josshua-DSA <joshuasyeba@gmail.com>
2026-09-10 08:33:06 +0700

    chore(preprocessing): export SlangNormalizer dan StopwordFilter

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,9 +1,17 @@
 # src/preprocessing/__init__.py
 from src.preprocessing.cleaner import TextCleaner
-from src.preprocessing.tokenizer import TextTokenizer
+from src.preprocessing.normalizer import SlangNormalizer
 from src.preprocessing.padder import SequencePadder
+from src.preprocessing.stopword_filter import StopwordFilter
+from src.preprocessing.tokenizer import TextTokenizer
 
-__all__ = ["TextCleaner", "TextTokenizer", "SequencePadder"]
+__all__ = [
+    "TextCleaner",
+    "SlangNormalizer",
+    "StopwordFilter",
+    "TextTokenizer",
+    "SequencePadder",
+]
 
 try:
     from src.preprocessing.splitter import DataSplitter
